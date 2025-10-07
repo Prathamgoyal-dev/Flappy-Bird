9df2555270c0eaaa5f600251d28877e3f66ba345
Prathamgoyal-dev <prathamgoyal.gamedev@gmail.com>
2025-10-08 01:52:52 +0530

    changes

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,13 +1,26 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class LogicScript : MonoBehaviour
 {
     public int PlayerScore = 0;
     public Text ScoreText;
+    public GameObject GameOverPanel;
 
-    public void IncrementScore(int scoretoAdd )
+    public void IncrementScore(int scoretoAdd)
     {
-        PlayerScore=PlayerScore+scoretoAdd;
+        PlayerScore = PlayerScore + scoretoAdd;
         ScoreText.text = PlayerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+    public void Gameover()
+    {
+        GameOverPanel.SetActive(true);
     }
 }
