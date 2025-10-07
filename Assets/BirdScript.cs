c5fc9cd2c536f7dbb70a063454c31c1c9d03371a
Prathamgoyal-dev <prathamgoyal.gamedev@gmail.com>
2025-10-07 23:50:24 +0530

    Update BirdScript.cs

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D myrigidbody;
     void Start()
     {
         gameObject.name = "Flappy Bird";
@@ -11,6 +12,6 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        myrigidbody.linearVelocity = Vector2.up * 10; 
     }
 }
